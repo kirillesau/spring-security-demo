037017b633d5773add3e790369172bda2d58e182
kirill <kirill-esau@outlook.de>
2022-04-22 21:56:11 +0200

    Demo Erweiterung

--- demo-api/src/main/java/de/kirill/demoapi/DemoController.java
+++ demo-api/src/main/java/de/kirill/demoapi/DemoController.java
@@ -7,26 +7,34 @@ import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContext;
 import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.oauth2.jwt.Jwt;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.Date;
+import java.util.StringJoiner;
 
 @RestController
-@RequestMapping(path = "demo")
+@RequestMapping(path = "callme/demo")
 @RequiredArgsConstructor
 @Slf4j
 public class DemoController {
 
-  @PreAuthorize("hasAuthority('SCOPE_TEST')")
+  @PreAuthorize(value = "hasAuthority('SCOPE_TEST')")
   @GetMapping
   public String getActualTime() {
     SecurityContext context = SecurityContextHolder.getContext();
     Authentication authentication = context.getAuthentication();
-    log.info("Scopes: " + authentication.getAuthorities());
-    log.info("Aktuelle Zeit wird ausgegeben");
-    return new Date().toString();
+    StringJoiner sj = new StringJoiner("\n\r");
+    String result = sj.add("Scopes: " + authentication.getAuthorities())
+        .add("Details: " + authentication.getDetails())
+        .add("getIssuedAt: " + ((Jwt) authentication.getPrincipal()).getIssuedAt())
+        .add("getExpiresAt: " + ((Jwt) authentication.getPrincipal()).getExpiresAt())
+        .add("Aktuelle Zeit: " + new Date())
+        .toString();
+    log.info(result);
+    return result;
   }
 
 }
