03aaf6c223566924c3cf447dce26c0d3c47fa4be
kirill <kirill-esau@outlook.de>
2022-04-22 21:55:39 +0200

    Actuator muss nicht authentifiziert werden

--- demo-api/src/main/java/de/kirill/demoapi/config/SecurityConfig.java
+++ demo-api/src/main/java/de/kirill/demoapi/config/SecurityConfig.java
@@ -12,8 +12,13 @@ import org.springframework.security.config.annotation.web.configurers.oauth2.ser
 @EnableGlobalMethodSecurity(prePostEnabled = true)
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
+  @Override
   protected void configure(HttpSecurity http) throws Exception {
-    http.authorizeRequests(authorize -> authorize.anyRequest().authenticated())
+    http.authorizeRequests(authorize -> authorize.antMatchers("/actuator/**")
+            .permitAll()
+            .anyRequest()
+            .authenticated())
         .oauth2ResourceServer(OAuth2ResourceServerConfigurer::jwt);
   }
+
 }
